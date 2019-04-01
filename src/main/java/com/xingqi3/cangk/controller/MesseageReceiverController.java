a9b101e491f44ba551775d03de0c868cc2d60dc4
星期三 <@DESKTOP-INUDOO8>
2019-04-01 22:46:20 +0800

    接受公众号发过来的信息并使用日志打印器打印输出

--- src/main/java/com/xingqi3/cangk/controller/MesseageReceiverController.java
+++ src/main/java/com/xingqi3/cangk/controller/MesseageReceiverController.java
@@ -1,15 +1,24 @@
 package com.xingqi3.cangk.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/sc/weixin/receiver")
 public class MesseageReceiverController{
-	@GetMapping
-	public String echo(
+	
+	private static final Logger LOG = (Logger) LoggerFactory.getLogger(MesseageReceiverController.class);
+
+	@GetMapping 
+	public String echo( 
 		String signature,
 		String timestamp,
 		String nonce,
@@ -17,4 +26,21 @@ public class MesseageReceiverController{
 		){
 		return echostr; 
 	}
+	@PostMapping
+	public String receice(
+			@RequestParam(value="signature",required=false)String signature,
+			@RequestParam(value="timestamp",required=false)String timestamp,
+			@RequestParam(value="nonce",required=false)String nonce,
+			@RequestBody String xml
+			)
+	{
+		LOG.trace("\n收到请求参数\n"
+				+"   signature:{}\n"
+				+"   timestamp:{}\n"
+				+"   nonce:{}\n"
+				+"收到的请求内容\n{}\n"
+				,signature,timestamp,nonce,xml);
+		return"success";		
+	};
+ 
 }
