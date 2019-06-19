3234316cc42f71a34597b4ba5e43aca8dadaceee
星期三 <@DESKTOP-INUDOO8>
2019-06-19 15:15:30 +0800

    rz

--- src/main/java/com/xingqi3/cangk/service/MessageService.java
+++ src/main/java/com/xingqi3/cangk/service/MessageService.java
@@ -1,8 +1,10 @@
 package com.xingqi3.cangk.service;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import com.xingqi3.cangk.domain.InMessage;
 import com.xingqi3.cangk.domain.OUtMessage;
-
+@EnableConfigurationProperties({MessageService.class})
 public interface MessageService {
 	OUtMessage onMessage(InMessage msg);
 }
