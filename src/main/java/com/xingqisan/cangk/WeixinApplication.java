1d8d6134d256f251ab263bed2552167a86dfda05
星期三 <@DESKTOP-INUDOO8>
2019-06-22 15:32:59 +0800

    日志

--- src/main/java/com/xingqisan/cangk/WeixinApplication.java
+++ src/main/java/com/xingqisan/cangk/WeixinApplication.java
@@ -3,7 +3,6 @@ package com.xingqisan.cangk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,24 +10,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 import com.xingqisan.cangk.domain.InMessage;
 import com.xingqisan.cangk.service.JsonRedisSerializer;
 
-
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class WeixinApplication {
 
+	// 相当于Spring的XML配置方式中的<bean>元素
 	@Bean
-	public RedisTemplate<String, InMessage> inMessageTemplate(
+	public RedisTemplate<String, InMessage> inMessageTemplate(//
 			@Autowired RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, InMessage> template = new RedisTemplate<>();
 		template.setConnectionFactory(redisConnectionFactory);
-		//设置一个序列化程序
+
 		template.setValueSerializer(new JsonRedisSerializer());
-		
+
+
 		return template;
-	
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeixinApplication.class, args);
 	}
